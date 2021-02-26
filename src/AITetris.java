7280de2cee419d3190e830b64786e0e07ff9dac6
brunozzz <16675056030>
2021-02-26 14:48:48 +0800

    Committed

--- src/AITetris.java
+++ src/AITetris.java
@@ -1,2 +1,28 @@
-public class AITetris extends Tetris{
+public class AITetris extends Tetris implements AI {
+
+//    JCheckBox debug
+
+    AITetris(int pixels) {
+        super(pixels);
+    }
+
+    @Override
+    public void tick(int direction) {
+        super.tick(direction);
+        if (direction == DOWN) {
+            gamingArea.undo();
+            Move move = calculateBestMove(gamingArea, currentShape);
+            if (move != null) {
+                if (move.x > newX) {
+                    super.tick(RIGHT);
+                } else if (move.x < newX) {
+                    super.tick(LEFT);
+                } else {
+                    super.tick(direction);
+                }
+                currentShape = move.shape;
+                score = (int) move.score;
+            }
+        }
+    }
 }
